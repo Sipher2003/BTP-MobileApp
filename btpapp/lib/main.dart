aebad9762cee290b9a7ed4f35e6f026f4d6031d0
Kaustubh <kaustubh.bhosale30@gmail.com>
2024-03-28 20:47:50 +0530

    new icon,appname and some fixes

--- btpapp/lib/main.dart
+++ btpapp/lib/main.dart
@@ -1,134 +1,7 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Football Player Market Value Prediction',
-//       theme: ThemeData(
-//         brightness: Brightness.dark, // Set background color to dark
-//         primaryColor: Colors.red, // Set slider color to red
-//       ),
-//       home: PredictionScreen(),
-//     );
-//   }
-// }
-
-// class PredictionScreen extends StatefulWidget {
-//   @override
-//   _PredictionScreenState createState() => _PredictionScreenState();
-// }
-
-// class _PredictionScreenState extends State<PredictionScreen> {
-//   // Default values for input features
-//   Map<String, double> defaultValues = {
-//     'Age': 23,
-//     'Dribbling / Reflexes': 92,
-//     'Passing / Kicking': 80,
-//     'Shooting / Handling': 89,
-//     'International reputation': 4,
-//     'Total mentality': 361,
-//     'Shot power': 88,
-//     'Total power': 410,
-//     'Ball control': 91,
-//     'Finishing': 93,
-//   };
-
-//   // Slider values for each feature
-//   Map<String, double> sliderValues = {};
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Initialize slider values with default values
-//     sliderValues = Map.from(defaultValues);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Predict Market Value of Football Players'),
-//       ),
-//       body: SingleChildScrollView(
-//         padding: EdgeInsets.all(16.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Container(
-//               decoration: BoxDecoration(
-//                 image: DecorationImage(
-//                   image: AssetImage(
-//                       'assets/images/banner.jpg'), // Add the banner image
-//                   fit: BoxFit.cover,
-//                 ),
-//               ),
-//               height: 200, // Adjust height as needed
-//             ),
-//             SizedBox(height: 16.0),
-//             ...defaultValues.keys.map((key) {
-//               return Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Text(key, style: TextStyle(color: Colors.white)),
-//                   Slider(
-//                     min: (key == 'Total mentality' || key == 'Total power')
-//                         ? 1
-//                         : 0,
-//                     max: (key == 'Total mentality' || key == 'Total power')
-//                         ? 500
-//                         : (key == 'International reputation')
-//                             ? 5
-//                             : 100,
-//                     divisions: 100,
-//                     value: sliderValues[key]!,
-//                     onChanged: (value) {
-//                       setState(() {
-//                         if (key == 'International reputation') {
-//                           // Round the value to the nearest integer for 'International reputation'
-//                           sliderValues[key] = value.roundToDouble();
-//                         } else {
-//                           sliderValues[key] = value;
-//                         }
-//                       });
-//                     },
-//                     activeColor: Theme.of(context).primaryColor,
-//                   ),
-//                   SizedBox(height: 8.0),
-//                   Text('Value: ${sliderValues[key]!.toStringAsFixed(0)}',
-//                       style: TextStyle(
-//                           color: Colors
-//                               .white)), // Use 0 decimal places for integer values
-//                   SizedBox(height: 16.0),
-//                 ],
-//               );
-//             }).toList(),
-//           ],
-//         ),
-//       ),
-//       bottomNavigationBar: Padding(
-//         padding: EdgeInsets.all(16.0),
-//         child: ElevatedButton(
-//           onPressed: () {
-//             // Handle prediction logic here
-//             print(sliderValues); // Print slider values
-//           },
-//           child: Text('Predict'),
-//         ),
-//       ),
-//     );
-//   }
-// }
-// Complete code for the Flutter application
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-
 
 void main() {
   runApp(MyApp());
@@ -175,6 +48,9 @@ class _PredictionScreenState extends State<PredictionScreen> {
   // Predicted value
   String? predictedValue;
 
+  // Loading state
+  bool isLoading = false;
+
   @override
   void initState() {
     super.initState();
@@ -186,7 +62,7 @@ class _PredictionScreenState extends State<PredictionScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Beyond The Pitch '),
+        title: Text('Beyond The Pitch'),
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(16.0),
@@ -207,10 +83,11 @@ class _PredictionScreenState extends State<PredictionScreen> {
               return Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(key, style: TextStyle(color: Colors.white)),
+                  if (key == 'Age') Text('Age', style: TextStyle(color: Colors.white)),
+                  if (key != 'Age') Text(key, style: TextStyle(color: Colors.white)),
                   Slider(
-                    min: (key == 'Total mentality' || key == 'Total power') ? 1 : 0,
-                    max: (key == 'Total mentality' || key == 'Total power') ? 500 : (key == 'International reputation') ? 5 : 100,
+                    min: (key == 'Age') ? 16 : ((key == 'Total mentality' || key == 'Total power') ? 1 : 0),
+                    max: (key == 'Age') ? 60 : ((key == 'Total mentality' || key == 'Total power') ? 500 : (key == 'International reputation') ? 5 : 100),
                     divisions: 100,
                     value: sliderValues[key]!,
                     onChanged: (value) {
@@ -231,6 +108,10 @@ class _PredictionScreenState extends State<PredictionScreen> {
                 ],
               );
             }).toList(),
+            if (isLoading)
+              Center(
+                child: CircularProgressIndicator(),
+              ),
             if (predictedValue != null)
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16.0),
@@ -244,15 +125,20 @@ class _PredictionScreenState extends State<PredictionScreen> {
         child: ElevatedButton(
           onPressed: () {
             // Call the Flask REST API to make predictions
+            setState(() {
+              isLoading = true;
+            });
             makePrediction(sliderValues).then((prediction) {
               print('$prediction');
               setState(() {
                 predictedValue = "Predicted value: ${prediction['prediction']} Euros";
+                isLoading = false;
               });
             }).catchError((error) {
               print('Error: $error');
               setState(() {
                 predictedValue = 'Error: $error';
+                isLoading = false;
               });
             });
           },
@@ -298,5 +184,3 @@ class _PredictionScreenState extends State<PredictionScreen> {
     }
   }
 }
-
-
